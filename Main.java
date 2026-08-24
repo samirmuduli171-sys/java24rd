d13dfd209005df55a5984da744dee53bdedfd870
samirmuduli171-sys <smuduli171@gmail.com>
2026-08-24 12:45:28 +0530

    few more topic of string

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 
 
-import java.util.Scanner;
+import java.util.*;
 
 
 public class Main {
@@ -37,5 +37,32 @@ public class Main {
         String sen = "My name is Samir";
         String name12 = sen.substring(11,16);
         System.out.println(name12);
+
+        //stringbuilder
+        StringBuilder sb = new StringBuilder("Hello");
+        System.out.println(sb);
+
+        //chat at index 0
+        System.out.println(sb.charAt(0));
+
+        //set char at index 0
+        sb.setCharAt(0,'P');
+        System.out.println(sb);
+
+        //insert at index 0
+        sb.insert(0,'S');
+        System.out.println(sb);
+
+        //insert at a specific location
+        sb.insert(5,'T');
+        System.out.println(sb);
+
+        //delete the extra
+        sb.delete(0,1);
+        System.out.println(sb);
+
+        //append
+        sb.append(" World");
+        System.out.println(sb);
     }
 }
